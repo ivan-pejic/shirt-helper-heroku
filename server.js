67e6956dff37d56c74dcaefa8ca66c0fb5903cc0
ivan-pejic <94369528+ivan-pejic@users.noreply.github.com>
2022-07-20 18:24:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get("/*", function (req, res) {
 
 // Add headers
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', 'https://completion.amazon.com/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
