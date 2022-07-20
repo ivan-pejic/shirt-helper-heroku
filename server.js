48588c59f1852a5999caa7b98b78eab09bf683b3
ivan-pejic <ivan_pejic_98@yahoo.com>
2022-07-20 18:42:53 +0200

    added cors

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
+app.use(cors());
 
 app.use(express.static(__dirname + "/dist"));
-app.get("/*", function (req, res) {
+app.get("/*", cors(), (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 // Add headers
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://completion.amazon.com/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://completion.amazon.com/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
 app.listen(process.env.PORT || 8080);
