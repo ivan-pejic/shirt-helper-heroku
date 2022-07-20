4ecf089cf003ac34dcc966b3d4229dc64d6294f5
ivan-pejic <94369528+ivan-pejic@users.noreply.github.com>
2022-07-20 14:42:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,13 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
+// Add headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.listen(process.env.PORT || 8080);
