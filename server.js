b50d0ae05eba14ec5349d64409b71e9fc8b762b5
ivan-pejic <94369528+ivan-pejic@users.noreply.github.com>
2022-07-21 14:09:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.all('/*', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://shirt-helper.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   //res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
