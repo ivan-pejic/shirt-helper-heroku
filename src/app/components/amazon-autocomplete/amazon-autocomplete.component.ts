50c443f4c36e03ffbaa6403b06ca83625df1afb7
ivan-pejic <94369528+ivan-pejic@users.noreply.github.com>
2022-07-21 14:35:46 +0200

    Update amazon-autocomplete.component.ts

--- src/app/components/amazon-autocomplete/amazon-autocomplete.component.ts
+++ src/app/components/amazon-autocomplete/amazon-autocomplete.component.ts
@@ -27,9 +27,6 @@ export class AmazonAutocompleteComponent implements AfterViewInit, OnDestroy {
 
   options = {
     headers: new HttpHeaders({
-      'Access-Control-Allow-Origin': 'https://shirt-helper.netlify.app/',
-      'Access-Control-Allow-Methods': 'GET',
-      'Access-Control-Allow-Headers': 'Accept',
       'Content-Type': 'application/x-www-form-urlencoded'
     }),
   };
