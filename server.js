da22a6c51710c9356269720c16335a76e6d4bdb5
ivan-pejic <94369528+ivan-pejic@users.noreply.github.com>
2022-07-21 13:23:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(cors());
 app.all('/*', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type');
+  //res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
