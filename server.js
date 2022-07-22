940985eb605f9cf23c9d9545750367edd1dcb283
ivan-pejic <94369528+ivan-pejic@users.noreply.github.com>
2022-07-22 09:57:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/tmhunt/", async (req, response) => {
   await axiosPost(
-    "http://tmhunt.com/ngrams.php",
+    //"http://tmhunt.com/ngrams.php",
+    "https://www.google.com/",
     qs.stringify({ query: req.query.query })
   ).then((res) => {
     response.send(res.data);
