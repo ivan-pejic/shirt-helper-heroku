85fb751d564d9deb4602b8f8a72e238b5aaad78f
ivan-pejic <ivan_pejic_98@yahoo.com>
2022-07-23 19:50:02 +0200

    fixed response

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const axiosGet = require("axios").get;
 const axiosPost = require("axios").post;
 const qs = require("qs");
+const { debug } = require("console");
 
 const app = express();
 
@@ -20,11 +21,11 @@ app.get("/tmhunt/", async (req, response) => {
   });
 });
 
-app.get("/amazon/", async (req, res) => {
+app.get("/amazon/", async (req, response) => {
   await axiosGet(
     `https://completion.amazon.com/search/complete?search-alias=aps&client=amazon-search-ui&mkt=1&q=/${req.query.query}`
   ).then((res) => {
-    res.send(res.data);
+    response.send(res.data);
   });
 });
 
@@ -32,5 +33,6 @@ app.use(express.static(__dirname + "/dist"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
-
+const port = process.env.port;
 app.listen(process.env.PORT || 8080);
+debug(app.port);
